dda5c8af84dbe6eb81804d1bde396475753ae7a1
Alexander <sasha.nest92@gmail.com>
2023-09-14 10:43:01 +0300

    remove extra new line

--- Pineapple.GrpcMock.Application/Common/Stub/Registry/IStubRegistry.cs
+++ Pineapple.GrpcMock.Application/Common/Stub/Registry/IStubRegistry.cs
@@ -5,6 +5,5 @@ namespace Pineapple.GrpcMock.Application.Common.Stub.Registry;
 public interface IStubRegistry
 {
     void Add(StubRegistryKeyDto key, StubRegistryValueDto value);
-
     StubRegistryValueDto? Get(StubRegistryKeyDto key);
 }
